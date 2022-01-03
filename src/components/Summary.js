4a056fd642dc84476fb7fc947b7e091a0a60294d
Shawn Rice <Shawn.CS@protonmail.com>
2022-01-03 15:52:33 -0500

    Update - Added selfie & summary component

---
+++ src/components/Summary.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Summary = () => {
+  return (
+    <div>
+      <p>Hi, I'm Shawn Rice</p>
+      <h2>I develop the web.</h2>
+      <p>I'm a full stack web developer specializing in UI/UX, CSS, React, Node, SQL, Python and RESTful APIs.</p>
+      <p>Currently, I'm a front-end engineer for <a href='https://spendmatters.com/'>Spend Matters</a></p>
+    </div>
+  )
+}
+
+export default Summary
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Landing from '../Landing'
 import Portfolio from '../Portfolio'
+import Summary from '../Summary'
 
 const Home = () => {
   return (
     <>
       <Landing />
+      <Summary />
       <Portfolio />
     </>
   )
